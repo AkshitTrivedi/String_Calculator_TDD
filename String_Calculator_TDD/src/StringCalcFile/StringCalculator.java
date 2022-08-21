509a8f54242f2af4f55825d03addf1ff930d036d
Akshit Trivedi <akkitrivedi2000@gmail.com>
2022-08-21 19:29:44 +0530

    Writing First Fail Test Case for Empty String

--- String_Calculator_TDD/src/StringCalcFile/StringCalculator.java
+++ String_Calculator_TDD/src/StringCalcFile/StringCalculator.java
@@ -2,5 +2,9 @@ package StringCalcFile;
 
 public class StringCalculator
 {
+    public static int addition(String s)
+    {
 
+        return 100;
+    }
 }
--- String_Calculator_TDD/src/TestStringCalcFile/TestStringCalculator.java
+++ String_Calculator_TDD/src/TestStringCalcFile/TestStringCalculator.java
@@ -1,7 +1,15 @@
 package TestStringCalcFile;
+import StringCalcFile.StringCalculator;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestStringCalculator
 {
-
+    //First Test Case to return 0 when Empty String is passed.
+    @Test
+    public void ReturnZeroForEmptyString()
+    {
+        assertEquals(0, StringCalculator.addition(""));
+    }
 }
