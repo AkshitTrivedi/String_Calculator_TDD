a877f331a9a7566557d2291ea4db6527ee03d088
Akshit Trivedi <akkitrivedi2000@gmail.com>
2022-08-21 21:29:27 +0530

    Testing the Unknown Number Sum by Passing Values in Main Method.

--- String_Calculator_TDD/src/Main.java
+++ String_Calculator_TDD/src/Main.java
@@ -13,5 +13,13 @@ public class Main
         //Passing 1&2 as String to the Function so that their sum will be returned.
         System.out.println(StringCalculator.add("1,2"));
 
+        //Passing 1,2,10 as String to the Function so that their sum will be returned.
+        System.out.println(StringCalculator.add("1,2,10"));
+
+        //Passing 100,200,300,400 as String to the Function so that their sum will be returned.
+        System.out.println(StringCalculator.add("100,200,300,400"));
+
+        //Passing 60,88,33,47,55,10,500 as String to the Function so that their sum will be returned.
+        System.out.println(StringCalculator.add("60,88,33,47,55,10,500"));
     }
 }
